e59ae562b34a797829758a42f907ba646936a12a
ganjiharitha <ganjiharitha789@gmail.com>
2021-06-22 11:54:49 +0530

    session

--- backend/config/config.js
+++ backend/config/config.js
@@ -1,4 +1,6 @@
 module.exports={
     mongopass : process.env.Mongoose_mini_password,
-    mongologin : process.env.Mongoose_mini_login
+    mongologin : process.env.Mongoose_mini_login,
+    MONGO_CONNECTION_STRING : process.env.stringmongo || '',
+    SESSION_SECRET:process.env.sessionsecrete || 'management'
 }
--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const app = express();
-
+var session = require( 'express-session' );
+var MongoStore = require( 'connect-mongo' );
 const apirouters = require('./backend/routers/apiroutes');
 const webrouters = require('./backend/routers/webroutes');
+var config = require('./backend/config/config');
 
 app.use(express.static(__dirname+"/frontend"));
 require("./connectionmongo");
-
+app.use( session( {
+    resave: false,
+    saveUninitialized: false,
+    secret: config.SESSION_SECRET,
+    store: MongoStore.create( {mongoUrl: config.MONGO_CONNECTION_STRING} )
+} ) );
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 
